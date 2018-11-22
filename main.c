26c3e4a2e4f06550495a3ea32c74abb01ba74920
wonrimjeong <wonrim0802@gmail.com>
2018-11-22 16:15:59 +0900

    5
    aa

--- main.c
+++ main.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#\
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	FILE *fp;
-	char input[100];
+	FILE *fp, *fp2;
+	char input;
+	char original[100];
+	char copy[100];
+	int i=0;
 	
-	fp = fopen("sample.txt", "r");
-	/*
+	printf("oringinal file : ");
+	scanf("%s", original);
+	printf("copy file : ");
+	scanf("%s", copy);
+	
+	fp = fopen(original, "r");
+	fp2 = fopen(copy, "w");
+	
 	while ( (input=fgetc(fp)) != EOF)
-		putchar(input); */
-		
-	while ( fgets(input, 100, fp) != NULL)
-		printf(input);
+	{
+		fputc(input, fp2);
+		i++;
+	}
+	
+	printf("Copy succeed~ (%i Bytes copied)\n", i);
 	
 	fclose(fp);
+	fclose(fp2);
 	
 	return 0;
 }
